83ff1e6ae2eb28d980919bf222ae6663a02c1b69
Christian Egli <christian@egli.cc>
2020-04-15 16:30:46 +0200

    Use tables that are readily available in the examples

--- examples/test.lua
+++ examples/test.lua
@@ -1,6 +1,6 @@
 #!/usr/bin/env lua
 
 local louis = require "liblua-louis"
-print(louis.translateString("de-ch-g2.ctb", "Güten Tag"))
-print(louis.translateString("de-ch-g2.ctb", "Guter Mann"))
-print(louis.translateString("de-ch-g2.ctb", "Gute Nacht"))
+print(louis.translateString("de-g2.ctb", "Güten Tag"))
+print(louis.translateString("de-g2.ctb", "Guter Mann"))
+print(louis.translateString("de-g2.ctb", "Gute Nacht"))
