c73a74c883ad138bc7e2934b4480c045a1df2f16
Christian Egli <christian@egli.cc>
2020-04-17 16:50:13 +0200

    Make the bindings compile with both lua5.1 and lua5.3

--- src/lua_louis.c
+++ src/lua_louis.c
@@ -89,7 +89,29 @@ static const struct luaL_Reg louis [] = {
   {NULL, NULL}  /* sentinel */
 };
 
+// provide backwards compat for lua5.1
+#if !defined LUA_VERSION_NUM || LUA_VERSION_NUM==501
+/*
+** Adapted from Lua 5.2.0
+*/
+static void luaL_setfuncs (lua_State *L, const luaL_Reg *l, int nup) {
+  luaL_checkstack(L, nup+1, "too many upvalues");
+  for (; l->name != NULL; l++) {  /* fill the table with given functions */
+    int i;
+    lua_pushstring(L, l->name);
+    for (i = 0; i < nup; i++)  /* copy upvalues to the top */
+      lua_pushvalue(L, -(nup+1));
+    lua_pushcclosure(L, l->func, nup);  /* closure with those upvalues */
+    lua_settable(L, -(nup + 3));
+  }
+  lua_pop(L, nup);  /* remove upvalues */
+}
+#endif
+
 int luaopen_louis (lua_State *L) {
-  luaL_openlib(L, "louis", louis, 0);
+  lua_newtable(L);
+  luaL_setfuncs(L, louis, 0);
   return 1;
+  /* luaL_openlib(L, "louis", louis, 0); */
+  /* return 1; */
 }
